9ed6d1b271945f653d7e393c75d3fc0ebb796a0f
Bilal1545 <abacar1243@gmail.com>
2025-03-06 18:47:24 +0300

    uptades

--- dotfiles/.config/bilaldot/scripts/change_wallpaper.sh
+++ dotfiles/.config/bilaldot/scripts/change_wallpaper.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-pkill -f hyprswitch
 # Kullanım: change_wallpaper.sh /path/to/wallpaper.png
 
 # Yeni duvar kağıdı yolu
-WALLPAPER_PATH="$1"
+file_path="$1"
 
-# Eğer parametre verilmediyse hata ver
-if [ -z "$WALLPAPER_PATH" ]; then
-    echo "Hata: Lütfen bir duvar kağıdı yolu belirtin!"
-    exit 1
-fi
 
 # Ön belleğe kaydetmek için dizin
 CACHE_DIR="$HOME/.cache/bilaldot"
@@ -19,13 +13,29 @@ CURRENT_WALLPAPER="$CACHE_DIR/current-wallpaper.png"
 # Dizin oluştur
 mkdir -p "$CACHE_DIR"
 
-# Yeni duvar kağıdını önbelleğe kopyala
+
+if [[ "$file_path" != *.png ]]; then
+    echo "Dosya PNG değil, dönüştürülüyor..."
+
+    # ImageMagick ile PNG'ye çevirme
+    convert "$file_path" "$new_path" && rm "$file_path"
+
+    # Eğer dönüştürme başarılı olduysa, yeni dosya yolunu değişkene kaydet
+    if [[ -f "$new_path" ]]; then
+        export WALLPAPER_PATH="$new_path"
+        echo "Dönüştürüldü ve yeni yol: $WALLPAPER_PATH"
+    else
+        echo "Dönüştürme başarısız!"
+        exit 1
+    fi
+else
+    WALLPAPER_PATH="$file_path"
+fi
+
 cp "$WALLPAPER_PATH" "$CURRENT_WALLPAPER"
 
 # Wallpaper'dan renkleri al ve uygula (hızlı mod, shell renklerini değiştirme)
 wal -i "$CURRENT_WALLPAPER" -n -s
 
-
 # `reload.sh` scriptini farklı bir shell ortamında bağımsız çalıştır
 hyprctl dispatch exec $HOME/.config/bilaldot/scripts/reload.sh
-
