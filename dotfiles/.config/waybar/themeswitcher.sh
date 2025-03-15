81609496d64f2bb98fbe36ae87bc7a0b7fe02f8b
Bilal1545 <abacar1243@gmail.com>
2025-03-15 16:11:18 +0300

    fixes

--- dotfiles/.config/waybar/themeswitcher.sh
+++ dotfiles/.config/waybar/themeswitcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 reload_script="$HOME/.config/bilaldot/scripts/reload.sh"
 # Rofi ile tema seçimi
-CHOICE=$(printf "Default\nGlass" | rofi -dmenu -replace -i \
+CHOICE=$(printf "Default\nGlass\nApple" | rofi -dmenu -replace -i \
     -config "$HOME/.config/rofi/config-themes.rasi" \
     -no-show-icons -width 30 -p "Select Theme:" -format i)
 
@@ -14,5 +14,9 @@ elif [[ "$CHOICE" == "1" ]]; then
     echo '@import url("../../waybar/themes/glass/style.css");' > ~/.config/bilaldot/settings/waybar-theme.css
     $reload_script
     notify-send "Theme Switcher" "Theme Changed to Glass Succesfully!"
+elif [[ "$CHOICE" == "2" ]]; then
+    echo '@import url("../../waybar/themes/apple/style.css");' > ~/.config/bilaldot/settings/waybar-theme.css
+    $reload_script
+    notify-send "Theme Switcher" "Theme Changed to Apple Succesfully!"
 fi
 
