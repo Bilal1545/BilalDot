0bce5849ab05c222edfcf6a75d4a8af0e0a2f1f7
Bilal1545 <abacar1243@gmail.com>
2025-07-16 12:38:33 +0300

    delete the WindOS

--- install.sh
+++ install.sh
@@ -25,27 +25,9 @@ else
     echo "Dependency installation skipped."
 fi
 
-# Shell selection
-read -p "Bi-Shell has 2 shells. Which one do you want to install? 1: Default  2: WindOS (1/2): " shell_choice
-
 # Copy .config directory to ~/.config
 echo -e "${green}Copying .config directories to ~/.config...${reset}"
 rsync -a .config/ ~/.config/
 
-# Copy shell config based on selection
-case "$shell_choice" in
-    1)
-        echo -e "${green}Installing Default shell...${reset}"
-        rsync -a default/ ~/.config/
-        ;;
-    2)
-        echo -e "${green}Installing WindOS shell...${reset}"
-        rsync -a WindOS/ ~/.config/
-        ;;
-    *)
-        echo "Invalid selection. Installation canceled."
-        exit 1
-        ;;
-esac
 
 echo -e "${green}Installation completed.${reset}"
